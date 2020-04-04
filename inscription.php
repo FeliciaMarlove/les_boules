4edc4077a78245348e9628afb8312547af8f32a7
FeliciaMarlove <missflorencemary@hotmail.com>
2020-04-04 15:17:54 +0200

    Saving user in database if email is not redundant

--- inscription.php
+++ inscription.php
@@ -3,11 +3,23 @@
 	try {
 		// récupère une instance du Singleton MyPdo pour avoir une seule connexion à la DB
 		$connexion = MyPdo::getInstance();
-		// vérifie que l'email n'est pas déjà enregistré en base de données
+		// vérifie que l'email récupéré du formulaire n'est pas déjà enregistré en base de données
 		$email = $_POST['email'];
+		// enregistre une requête préparée pour le select
 		$reqPreparee = $connexion->prepare("SELECT * FROM TBL_CLIENT WHERE EMAIL = ?");
-		$reqPreparee->execute(array("$email"));
-		echo $reqPreparee->rowCount();
+		//exécute la requête préparée avec les infos passées en paramètre pour remplacer le "?"
+		$reqPreparee->execute(array(strtolower(trim("$email"))));
+		// si la requête renvoie des résultats (rowCount > 0), l'e-mail est déjà en base de données
+		if ($reqPreparee->rowCount() > 0) {
+
+		// sinon, on enregistre dans la base de données
+		} else {
+			$date = new DateTime();
+			$timeStamp = $date->getTimestamp();
+			$reqPreparee = $connexion->prepare("INSERT INTO `TBL_CLIENT`(`NOM_PRENOM`, `ADRESSE`, `ID_VILLE`, `EMAIL`, `PASSWORD`, `OPTIN`) VALUES (?,?,?,?,?,?)");
+			// exécute la requête préparée avec les informations récupérées dans le POST depuis le formulaire HTML
+			$reqPreparee->execute(array($_POST['nom'],$_POST['adresse'],$_POST['localite'],$_POST['email'],$_POST['motdepasse'],$timeStamp));
+		}
 		
 	} catch(PDOException $e) {
 		exit("Erreur ouverture BD : ".$e->getMessage());
