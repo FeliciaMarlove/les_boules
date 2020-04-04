00aeb357aba212a0b70835839d74b78f03819a7d
FeliciaMarlove <missflorencemary@hotmail.com>
2020-04-04 15:23:41 +0200

    Navigating to either eshop or sign up page from inscription.php

--- inscription.php
+++ inscription.php
@@ -19,6 +19,7 @@
 			$reqPreparee = $connexion->prepare("INSERT INTO `TBL_CLIENT`(`NOM_PRENOM`, `ADRESSE`, `ID_VILLE`, `EMAIL`, `PASSWORD`, `OPTIN`) VALUES (?,?,?,?,?,?)");
 			// exécute la requête préparée avec les informations récupérées dans le POST depuis le formulaire HTML
 			$reqPreparee->execute(array($_POST['nom'],$_POST['adresse'],$_POST['localite'],$_POST['email'],$_POST['motdepasse'],$timeStamp));
+			header('Location: ./eshop.php');
 		}
 		
 	} catch(PDOException $e) {
